b5beb62dd3aeba53adffb65ef62cea650ca6dcf4
rubcuadra <rubcuadra@gmail.com>
2018-09-18 14:26:49 -0500

    debugging

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from charts import run as chart
 if __name__ == '__main__':
     dumpType = types.CSV
     subreddits = ["politics","news","The_Donald","truenews","PoliticalHumor","democrats","all"] #"ElectionPolls", "Ask_Politics"
-    keyWords   = ["latino", "hispanic","immigrant","realdonaldtrump","texas","daca","immigration","families","register","trump","children","immigrants",'dreamers'] #"misinformation", "midterm" 
+    # keyWords   = ["latino", "hispanic","immigrant","realdonaldtrump","texas","daca","immigration","families","register","trump","children","immigrants",'dreamers'] #"misinformation", "midterm" 
+    keyWords   = ["latino", "hispanic"] #"misinformation", "midterm" 
     oldest_post_date = "10/09/2017"  #dd/mm/yyyy
     
     #Create CSV files with posts
